34f1e474494aeac1bf836ad2ef902ab4aedc7d39
RTids <121754534+RTids@users.noreply.github.com>
2023-01-13 17:15:02 +0000

    added color changes via JS listener

--- index.js
+++ index.js
@@ -3,11 +3,16 @@ let navName = document.getElementById('header-name')
 let dropBtnEl = document.getElementById('drop-btn')
 
 dropBtnEl.addEventListener('click', function(){
-    
-    if (navName.textContent === 'PARTY TIME!!') {
+    const bodyEl = document.querySelector('.body')
+    const navEl = document.querySelector('nav')
+    if (navName.textContent === 'party time...') {
         navName.textContent = 'welcome...'
+        navEl.style.backgroundColor = 'rgb(27, 26, 26)'
+        bodyEl.style.backgroundColor = 'rgb(33, 32, 32)'
     } else {
-        navName.textContent = 'PARTY TIME!!'
+        navName.textContent = 'party time...'
+        navEl.style.backgroundColor = 'lightblue'
+        bodyEl.style.backgroundColor = 'pink'
     }
 }
 )
